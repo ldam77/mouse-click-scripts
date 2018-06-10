ca716ebe9fddcd8a7c243d4e8ba902e456b83c64
Lan Dam <ldam77@yahoo.com>
2018-06-10 12:51:29 -0700

    adds scripts to change images and random cpu pick

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,49 @@
+// function showImage(src, location) {
+//     var img = document.createElement("img");
+//     img.src = src;
+//     img.alt = src;
+//     document.getElementById(location).appendChild(img);
+// }
+
+function changeImage(source, location){
+  document.getElementById(location).src = source;
+}
+
 function showRock(event) {
-    $("#user-selection").text("You chose Rock!!!");
+  changeImage('img/Rock.png', 'your-pick');
+    rockPaperScissorResult("rock");
 }
 
 function showPaper(event) {
-    $("#user-selection").text("You chose Paper!!!");
+  changeImage('img/Paper.png', 'your-pick');
+    rockPaperScissorResult("paper");
 }
 
 function showScissor(event) {
-    $("#user-selection").text("You chose Scissor!!!");
+  changeImage('img/Scissor.png', 'your-pick');
+    rockPaperScissorResult("scissor");
+}
+
+// get a random number between 0 and max-1
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function rockPaperScissorResult(userPick){
+  var result = "You Win!!!";
+  var rpsChoices = ["Rock", "Paper", "Scissor"];
+  var cpuPick = rpsChoices[getRandomInt(3)];
+
+  if(cpuPick === "Rock"){
+    changeImage('img/Rock.png', 'cpu-pick');
+  } else if(cpuPick === "Paper"){
+    changeImage('img/Paper.png', 'cpu-pick');
+  }else {
+    changeImage('img/Scissor.png', 'cpu-pick');
+  };
+
+  $("#output1").text("You chose " + userPick + ". I chose " + cpuPick + ".");
+  $("#output2").text(result);
 }
 
 function showCoords1(event) {
